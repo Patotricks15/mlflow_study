1efc10c34c44e9630d8b66fc9db9fa84261d1b03
Patotricks <patrickufrrj@gmail.com>
2025-03-26 10:46:13 -0300

    1 model -> 3 models

--- iris.py
+++ iris.py
@@ -4,6 +4,8 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 
 mlflow.set_tracking_uri("http://127.0.0.1:8080/")
 mlflow.set_experiment("iris")
@@ -14,14 +16,21 @@ X, y = iris.data, iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
+modelos = [
+    ("RandomForest", RandomForestClassifier(n_estimators=100, random_state=42)),
+    ("LogisticRegression", LogisticRegression(max_iter=200, random_state=42)),
+    ("SVC", SVC(gamma='scale', random_state=42))
+]
 
-y_pred = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print("Acurácia do modelo:", accuracy)
+for nome, model in modelos:
+    print(nome)
+    model.fit(X_train, y_train)
 
-with mlflow.start_run():
-    mlflow.log_param("n_estimators", 100) # log parameter
-    mlflow.log_metric("accuracy", accuracy) # log metric
-    mlflow.sklearn.log_model(model, "model_random_forest") # log model
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Acurácia do modelo:", accuracy)
+
+    with mlflow.start_run():
+        mlflow.log_param("model", model) # log parameter
+        mlflow.log_metric("accuracy", accuracy) # log metric
+        mlflow.sklearn.log_model(model, "nome") # log model
